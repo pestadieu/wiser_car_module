f38a16d4fcd341a63a80dd334fa069bd71196371
ESTADIEU Paul-louis <pestadieu@barr-find.pedago.ipb.fr>
2017-10-27 16:10:00 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import socket
+import threading
+
+class server(threading.Thread):
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.name = "server"
+		
+	def run(self):
+      print "Starting TCP server on " + 
+	
+	def listen(self, interface):
+	
+	
+	
