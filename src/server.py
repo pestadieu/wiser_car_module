dd82e3b4fa5ba749908e98fb99b7da8f9fc5377b
jojoH2407 <joseph.kangou@gmail.com>
2017-12-15 16:43:21 +0100

    add gpio.cleanup() method to avoid court-circuit

--- src/server.py
+++ src/server.py
@@ -28,6 +28,7 @@ class handle_led(Thread):
                         time.sleep(1)
                         GPIO.output(18, GPIO.LOW)
                         time.sleep(1)
+                GPIO.cleanup()
                 return
 
 class Serv(BaseHTTPRequestHandler):
